dde54abcb4811222aa00f2049b1cf55eb3d0b5bf
coethan04 <ethanmco@gmail.com>
2026-02-10 15:35:47 -0500

    Update Mapbox access token and data sources

--- script.js
+++ script.js
@@ -1,152 +1,65 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiY29ldGhhbiIsImEiOiJjbWt5ajd0ZmUwOGhuM21wd3lwcTNnZTA5In0.WEiOusvvz_Xz4b7cNisNUQ";
-
+mapboxgl.accessToken = "pk.eyJ1IjoiY29ldGhhbiIsImEiOiJjbWxneDdjdmUwMzZ1M2RwdXV6cW1tYjBmIn0.IWKAo1T_kI9KvHA_LCdc8g";
 const map = new mapboxgl.Map({
-    container:"map" ,
-    style:"mapbox://styles/coethan/cml8xzlck00a001s4hovde6c9",
-    center:[43.653,-79.369],
-    zoom: "12.80"
+    container: "my-map",
+    style: "mapbox://styles/coethan/cml8xzlck00a001s4hovde6c9",
+    center: [-79.369, 43.653],
+    zoom: 12.80
 });
-
 
 map.on('load', () => {
 
-    map.addSource('uoft-data',{
+    map.addSource('toronto-data', {
         type: 'geojson',
-        data: {
-"type": "FeatureCollection",
-"features": [
-{
-"type": "Feature",
-"properties": {
-"name": "Roy Thompson Hall",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.386443,
-43.646615
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Toronto City Hall",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.3841,
-43.6534
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Toronto Metropolitan University",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.3792,
-43.6577
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Royal Ontario Museum",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.3948,
-43.6677
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Distillery District",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.359222,
-43.650238
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "CN Tower",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.3871,
-43.6426
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Randolph Theatre",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.4109113,
-43.6635654
-],
-"type": "Point"
-}
-},
-{
-"type": "Feature",
-"properties": {
-"name": "Tim Hortons",
-"marker-color": "#1b2ebb",
-"marker-size": "medium"
-},
-"geometry": {
-"coordinates": [
--79.39,
-43.66
-],
-"type": "Point"
-}
-}
-]
-}
-    })
-})
-
-
-map.addLayer({
-    'id': 'uoft-pnt',
-    'type': 'circle',
-    'source': 'uoft-data',
-    'paint': {
-        'circle-radius': 6,
-        "circle-color": '#B42222'
-    }
-})
+        data: 'https://raw.githubusercontent.com/coethan04/Lab2GGR472/refs/heads/main/map.geojson'
+    });
+    map.addLayer({
+        'id': 'toronto-point',
+        'type': 'circle',
+        'source': 'toronto-data',
+        'paint': {
+            'circle-radius': 7,
+            'circle-color': '#B41229'
+        }
+    });
+    map.addSource('city-data', {
+        'type': 'vector',
+        'url': 'mapbox://coethan.8ft8j96j'
+    });
+    map.addLayer({
+        'id': 'city points',
+        'type': 'circle',
+        'source': 'city-data',
+        'paint': {
+            'circle-color': '#3810c8ff',
+            'circle-radius': 7,
+        },
+        'source-layer': 'map-37wnnq'
+    },
+        'toronto-point');
+});
+map.on('style.load', () => {
+    map.setConfigProperty('basemap', 'lightPreset', 'dawn');
+    const zoomBasedReveal = (value) => {
+        return [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            11,
+            0.0,
+            13,
+            value
+        ];
+    };
+    map.setRain({
+        density: zoomBasedReveal(0.5),
+        intensity: 1.0,
+        color: '#a8adbc',
+        opacity: 0.7,
+        vignette: zoomBasedReveal(1.0),
+        'vignette-color': '#464646',
+        direction: [0, 80],
+        'droplet-size': [2.6, 18.2],
+        'distortion-strength': 0.7,
+        'center-thinning': 0 
+    });
+});
